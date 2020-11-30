117dad34c4e917d500aa9d3824f22ca10462f1cb
Иван Дашкевич <spaceorc@yandex.ru>
2020-11-30 16:07:51 +0500

    Initial commit

---
+++ aoc/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Threading;
+using System.Threading.Tasks;
+using Experiments;
+using MoreLinq.Extensions;
+
+namespace aoc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("/Users/spaceorc/Downloads/input.txt");
+
+            Console.Out.WriteLine("result");
+        }
+    }
+}
