278220c1f4b26e8f326c523c6a3064069b5ae8de
Ivan Dashkevich <spaceorc@yandex.ru>
2020-12-02 17:46:18 +0500

    Update Program.cs

--- aoc/Program.cs
+++ aoc/Program.cs
@@ -77,8 +77,8 @@ namespace aoc
         {
             var numbers = File.ReadAllLines("/Users/spaceorc/Downloads/input.txt").Select(long.Parse).ToArray();
 
-            for (int i = 0; i < numbers.Length - 2; i++)
-            for (int j = i + 1; j < numbers.Length - 1; j++)
+            for (int i = 0; i < numbers.Length - 1; i++)
+            for (int j = i + 1; j < numbers.Length; j++)
             {
                 if (numbers[i] + numbers[j] == 2020)
                     Console.Out.WriteLine(numbers[i] * numbers[j]);
